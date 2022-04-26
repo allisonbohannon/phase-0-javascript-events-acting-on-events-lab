5ea66513b118d430a0fca4fec1bdd62e57c6dd66
Allison Bohannon <akboh.github@gmail.com>
2022-04-25 18:46:52 -0700

    commit tree 341911eaedf59a50834def9566467dfbfca7bc23

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+
+
+document.addEventListener('keydown', function (event) {
+    if (event.key === 'ArrowLeft') {
+        moveDodgerLeft(); 
+    } else if (event.key === 'ArrowRight') {
+        moveDodgerRight(); 
+    }
+} )
+
+
+function moveDodgerLeft() {
+    const dodger = document.getElementById('dodger'); 
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+      
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }; 
+    
+}; 
+function moveDodgerRight() {
+    const dodger = document.getElementById('dodger'); 
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+      
+    if (left < 360) {
+        dodger.style.left = `${left + 1}px`;
+    }; 
+    
+}; 
